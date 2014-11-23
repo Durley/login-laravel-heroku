2969dc943f71948857f7d9c4c78e8960bdb3d8de
Durley <alarcondurley13@gmail.com>
2014-11-23 05:20:56 +0000

    heroku laravel

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -1,18 +1,27 @@
-<div class="container">
+@extends ('admin/layout')
 
+@section ('title') Login @stop
+
+@include ('admin/users/errors', array('errors' => $errors))
+
+@section ('content')
+
+<h2 class="">Por favor, inicia sesión</h2>
+<div class="container">
 <form class="form-signin form-horizontal" method="post" action="/login">
-<h2 class="">Please sign in</h2>
-<div class="control-group ">
-<label class="control-label" for="email">Email:</label>
-<div class="controls">
-<input type="text" id="email" name="email" placeholder="Email address" value="">
-</div>
-</div>
-<div class="control-group ">
-<label class="control-label" for="password">Password:</label>
-<div class="controls">
-<input type="password" id="password" name="password" placeholder="Password" value="">
-</div>
+
+    <div class="row">
+        <div class="form-group col-md-4">
+            {{ Form::label('email', 'Dirección de E-mail') }}
+            {{ Form::text('email', null, array('placeholder' => 'Introduce tu E-mail', 'class' => 'form-control')) }}
+        </div>
+    </div>
+    <div class="row">
+        <div class="form-group col-md-4">
+            {{ Form::label('password', 'Contraseña') }}
+            {{ Form::password('password', array('placeholder' => 'Contraseña', 'class' => 'form-control')) }}
+        </div>
+    </div>
 </div>
 <button class="btn btn-large btn-primary" type="submit">Sign in</button>
     <a href="{{ route('admin.users.create') }}" class="btn btn-info">
